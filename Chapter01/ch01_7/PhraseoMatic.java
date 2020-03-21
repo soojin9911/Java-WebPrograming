9b0972c8b1c176ed00fb17b436440c6ad170e05b
soojin9911 <56923169+soojin9911@users.noreply.github.com>
2020-03-21 21:15:17 +0900

    Create PhraseoMatic.java

---
+++ Chapter01/ch01_7/PhraseoMatic.java
@@ -0,0 +1,24 @@
+package ch01_7;
+
+public class PhraseoMatic {
+    public static void main(String[] args) {
+        System.out.println("Hello World!, 차수진_2018250051");
+        String[] wordListOne = {"24/7","multi-Test","30,000 foot","B-to-B",
+                "win-win","front-end","web-based","pervasive","smart","six-sigma",
+                "critical-path","dynamic"};
+        String[] wordListTwo = {"empowered","sticky","value-added","oriented","centric",
+                "distributed","clustered","branded","outside-the-box","positioned","networked",
+                "focused","leveraged","aligned","targeted","shared","cooperative","accelerated"};
+        String[] wordListThree = {"process","tipping-point","solution","architecture","core competency",
+                "strategy","mindshare","portal","space","vision","paradigm","mission"};
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+        int rand1 = (int)(Math.random() * oneLength);
+        int rand2 = (int)(Math.random() * twoLength);
+        int rand3 = (int)(Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListOne[rand2] + " " + wordListThree[rand3];
+        System.out.println("What we need is a "+phrase);
+    }
+}
