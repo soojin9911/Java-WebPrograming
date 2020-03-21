6e99daff8e2590c4070ef466d77497cdf4f6281a
soojin9911 <56923169+soojin9911@users.noreply.github.com>
2020-03-21 20:58:56 +0900

    Add files via upload

---
+++ Loopy.java
@@ -0,0 +1,17 @@
+package ch01_1;
+
+public class Loopy {
+    public static void main(String[] args) {
+        System.out.println("Hello World!, 차수진_2018250051");
+        int x = 1;
+        System.out.println("Before the Loop");
+        while (x < 4) {
+            System.out.println("In the loop");
+            System.out.println("Value of x is " + x);
+            x = x + 1;
+        }
+        System.out.println("This is after the loop");
+
+    }
+}
+
