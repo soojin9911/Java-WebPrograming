0df39d1115fa3de21d0c4b2068ea90985b586e73
soojin9911 <56923169+soojin9911@users.noreply.github.com>
2020-03-21 21:12:06 +0900

    Create IfTest2.java

---
+++ Chapter01/ch01_2/IfTest2.java
@@ -0,0 +1,14 @@
+package ch01_2;
+
+public class IfTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!, 차수진_2018250051");
+        int x = 2;
+        if(x == 3) {
+            System.out.println("x must be 3");
+        }else {
+            System.out.println("x is NOT 3");
+        }
+        System.out.println("THis runs no matter what");
+    }
+}
